c55d088106b0b0fdbe68f4ac7facfddfe4d13067
Lindsey <lindsey_mccluskey@yahoo.ca>
2025-06-04 12:08:14 -0700

    tailwind and vite working

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,14 @@
 import {defineConfig} from 'vite'
 import tailwindcss from '@tailwindcss/vite'
+import react from "@vitejs/plugin-react"
 
 export default defineConfig({
     plugins: [
-        tailwindcss()
-    ]
+        tailwindcss(),
+        react()
+    ],
+    server: {
+        host: true,
+        port: 8000,
+      }
 })

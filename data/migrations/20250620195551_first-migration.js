c4f09552f77a843f4f354da3790f6909488d4107
Lindsey <lindsey_mccluskey@yahoo.ca>
2025-06-20 13:02:55 -0700

    db working

---
+++ data/migrations/20250620195551_first-migration.js
@@ -0,0 +1,18 @@
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.up = async function(knex) {
+    await knex.schema.createTable('personnel', table => {
+        table.increments('personnel_id')
+    })
+  
+};
+
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.down = async function(knex) {
+  await knex.schema.dropTableIfExists('personnel')
+};
--- eslint.config.js
+++ eslint.config.js
@@ -6,7 +6,8 @@ import pluginReact from "eslint-plugin-react";
 /** @type {import('eslint').Linter.Config[]} */
 export default [
   {files: ["**/*.{js,mjs,cjs,jsx}"]},
-  {languageOptions: { globals: {...globals.browser, ...globals.node} }},
+  {files: ["**/*.js"], languageOptions: {sourceType: "commonjs"}},
+  {languageOptions: { globals: globals.node }},
   pluginJs.configs.recommended,
   pluginReact.configs.flat.recommended,
 ];

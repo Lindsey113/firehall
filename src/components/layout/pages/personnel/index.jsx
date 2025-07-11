410bb268c9d342d843ee19ad7131a0dedc37a884
Lindsey <lindsey_mccluskey@yahoo.ca>
2025-07-11 13:24:33 -0700

    starting personnel page

--- src/components/layout/pages/personnel/index.jsx
+++ src/components/layout/pages/personnel/index.jsx
@@ -4,6 +4,62 @@ export const PersonnelLandingPage = () => {
     <div>
       <section>
         <h1>Personnel Page</h1>
+        <div className="m-10">
+        <form className="flex flex-col">
+          <input
+          placeholder="First name"
+          ></input>
+          <input
+          placeholder="Middle name"
+          ></input>
+          <input
+          placeholder="Last name"
+          ></input>
+          <input
+          placeholder="Pay class"
+          ></input>
+          <input
+          placeholder="Address"
+          ></input>
+          <input
+          placeholder="Address 2 (optional)"
+          ></input>
+          <input
+          placeholder="City"
+          ></input>
+          <input
+          placeholder="Province"
+          ></input>
+          <input
+          placeholder="Postal Code"
+          ></input>
+          <input
+          placeholder="Country"
+          ></input>
+          <input
+          placeholder="Home phone"
+          ></input>
+          <input
+          placeholder="Mobile Phone"
+          ></input>
+          <input
+          placeholder="Mobile provider"
+          ></input>
+          <input
+          placeholder="Work phone"
+          ></input>
+          <input
+          placeholder="Email"
+          ></input>
+          <input
+          placeholder="Related records"
+          ></input>
+          <input
+          placeholder="Notes"
+          ></input>
+
+        </form>
+        </div>
       </section>
     </div>
   )

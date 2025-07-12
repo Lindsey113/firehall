3ad7e36ba5a5cc13905f2fc52fff214f69efe89e
Lindsey <lindsey_mccluskey@yahoo.ca>
2025-07-12 12:53:05 -0700

    working on personnel form

--- src/components/layout/pages/personnel/index.jsx
+++ src/components/layout/pages/personnel/index.jsx
@@ -3,62 +3,65 @@ export const PersonnelLandingPage = () => {
   return (
     <div>
       <section>
-        <h1>Personnel Page</h1>
+        <h1 className="flex m-10 font-bold text-xl">Add New Personnel:</h1>
         <div className="m-10">
-        <form className="flex flex-col">
-          <input
+        <form className="flex flex-col border p-4 gap-4 items- ">
+          <input className="border"
           placeholder="First name"
           ></input>
-          <input
+          <input className="border"
           placeholder="Middle name"
           ></input>
-          <input
+          <input className="border"
           placeholder="Last name"
           ></input>
-          <input
+          <input className="border"
           placeholder="Pay class"
           ></input>
-          <input
+          <input className="border"
           placeholder="Address"
           ></input>
-          <input
+          <input className="border"
           placeholder="Address 2 (optional)"
           ></input>
-          <input
+          <input className="border"
           placeholder="City"
           ></input>
-          <input
+          <input className="border"
           placeholder="Province"
           ></input>
-          <input
+          <input className="border"
           placeholder="Postal Code"
           ></input>
-          <input
+          <input className="border"
           placeholder="Country"
           ></input>
-          <input
+          <input className="border"
           placeholder="Home phone"
           ></input>
-          <input
+          <input className="border"
           placeholder="Mobile Phone"
           ></input>
-          <input
+          <input className="border"
           placeholder="Mobile provider"
           ></input>
-          <input
+          <input className="border"
           placeholder="Work phone"
           ></input>
-          <input
+          <input className="border"
           placeholder="Email"
           ></input>
-          <input
+          <input className="border"
           placeholder="Related records"
           ></input>
-          <input
+          <input className="border"
           placeholder="Notes"
           ></input>
-
         </form>
+        </div>
+        <div className="flex gap-5 m-10 flex-col">
+          <button className="logging-btn">Add</button>
+          <button className="logging-btn">Select Existing Personnel</button>
         </div>
       </section>
     </div>

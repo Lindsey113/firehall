f8399bd5ab969a6bcd0c1c66657e0e7eb23b7678
Lindsey <lindsey_mccluskey@yahoo.ca>
2025-10-03 13:34:36 -0700

    wip

--- src/components/layout/pages/authenticated/index.jsx
+++ src/components/layout/pages/authenticated/index.jsx
@@ -4,10 +4,7 @@ import { useAuth0 } from "@auth0/auth0-react";
 export const AuthPage = () => {
     const { user, isLoading } = useAuth0()
 
-
     if (isLoading) return <div>Loading...</div>
-
-    
 
     return (
         <div className="flex justify-left">
